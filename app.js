0b3ac422640e68279d9de32f0227da47632d6d4a
Adriana Black <adrianablancov@gmail.com>
2020-10-18 10:11:11 +0100

    added create tradesman endpoint

---
+++ app.js
@@ -0,0 +1,17 @@
+// const express = require('express')
+// const path = require('path')
+// const _ = require('lodash')
+// const bodyParser = require('body-parser')
+// const fs = require('fs')
+
+// const routes = require('./routes')
+
+// const app = express()
+
+// app.use(bodyParser.json());
+
+// app.use(express.urlencoded({ extended: false }));
+
+// app.use('/tradesmen', routes.tradesmen);
+
+// module.exports = app;
--- server.js
+++ server.js
@@ -6,16 +6,44 @@ const fs = require('fs')
 
 const routes = require('./routes')
 
+const tradesmen = require('./tradesMen.json')
+
 const app = express()
 const port = 5000
 
+app.use(express.urlencoded({ extended: true }));
 app.use(bodyParser.json());
-app.use(express.urlencoded({ extended: false }));
 
+
+const loadTradesmen = () => {
+    try {
+        const dataBuffer = fs.readFileSync('tradesMen.json')
+        const dataJson = dataBuffer.toString()
+        return JSON.parse(dataJson)
+    } catch (e) {
+        console.log(`${e}: ERROR:Failed to load tradesMen.json`)
+        return []
+    }
+   
+}
+
+const saveTradesMen = (tradesmen) => {
+    const dataJSON = JSON.stringify(tradesmen)
+    fs.writeFileSync('tradesMen.json', dataJSON)
+}
 
 app.get('/tradesmen', routes.tradesmen);
 app.post('/tradesmen', (req, res) => {
-
+    const { id } = req.body
+    const tradesmen = loadTradesmen()
+    const duplicateTradesman = tradesmen.find((tradesman) => tradesman.id === id)
+    if (!duplicateTradesman) {
+        tradesmen.push({
+         ...req.body
+        })
+    }
+    saveTradesMen(tradesmen)
+    tradesmen.push(req.body)
 res.send(req.body)
 })
 
